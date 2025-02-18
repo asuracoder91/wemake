f7d4f05ca6ac1370a5a62197ebcbea40cc1ea642
asuracoder91 <asuracoder@gmail.com>
2025-02-18 23:26:05 +0900

    3.6 Post Card

--- app/common/components/navigation.tsx
+++ app/common/components/navigation.tsx
@@ -10,6 +10,25 @@ import {
 	navigationMenuTriggerStyle,
 } from "./ui/navigation-menu";
 import { cn } from "~/lib/utils";
+import { Button } from "./ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+	BarChart3Icon,
+	BellIcon,
+	LogOutIcon,
+	MessageCircleIcon,
+	SettingsIcon,
+	UserIcon,
+} from "lucide-react";
 
 const menus = [
 	{
@@ -122,7 +141,15 @@ const menus = [
 	},
 ];
 
-export default function Navigation() {
+export default function Navigation({
+	isLoggedIn,
+	hasNotifications,
+	hasMessages,
+}: {
+	isLoggedIn: boolean;
+	hasNotifications: boolean;
+	hasMessages: boolean;
+}) {
 	return (
 		<nav className="flex px-20 h-16 items-center justify-between backdrop-blur fixed top-0 left-0 right-0 z-50 bg-background/50">
 			<div className="flex items-center">
@@ -160,7 +187,7 @@ export default function Navigation() {
 															asChild
 														>
 															<Link
-																className="p-3 bg-primary space-y-1 block leading-none no-underline outline-none"
+																className="p-3 space-y-1 block leading-none no-underline outline-none"
 																to={item.to}
 															>
 																<span className="text-sm text-primary-foreground font-medium leading-none">
@@ -191,6 +218,89 @@ export default function Navigation() {
 					</NavigationMenuList>
 				</NavigationMenu>
 			</div>
+			{isLoggedIn ? (
+				<div className="flex items-center gap-2">
+					<Button
+						size="icon"
+						variant="ghost"
+						asChild
+						className="relative"
+					>
+						<Link to="/my/notifications">
+							<BellIcon className="size-4" />
+							{hasNotifications && (
+								<div className="absolute top-1.5 right-1.5 size-2 bg-red-500 rounded-full" />
+							)}
+						</Link>
+					</Button>
+					<Button
+						size="icon"
+						variant="ghost"
+						asChild
+						className="relative"
+					>
+						<Link to="/my/messages">
+							<MessageCircleIcon className="size-4" />
+							{hasMessages && (
+								<div className="absolute top-1.5 right-1.5 size-2 bg-red-500 rounded-full" />
+							)}
+						</Link>
+					</Button>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Avatar>
+								<AvatarImage src="https://github.com/asuracoder91.png" />
+								<AvatarFallback>CN</AvatarFallback>
+							</Avatar>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent className="w-56">
+							<DropdownMenuLabel className="flex flex-col">
+								<span className="font-medium">Andrew Lee</span>
+								<span className="text-xs text-muted-foreground">
+									@asuracoder91
+								</span>
+							</DropdownMenuLabel>
+							<DropdownMenuSeparator />
+							<DropdownMenuGroup>
+								<DropdownMenuItem asChild>
+									<Link to="/my/dashboard">
+										<BarChart3Icon className="size-4 mr-2" />{" "}
+										Dashboard
+									</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem asChild>
+									<Link to="/my/profile">
+										<UserIcon className="size-4 mr-2" />{" "}
+										Profile
+									</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem asChild>
+									<Link to="/my/settings">
+										<SettingsIcon className="size-4 mr-2" />{" "}
+										Settings
+									</Link>
+								</DropdownMenuItem>
+							</DropdownMenuGroup>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem asChild>
+								<Link to="/auth/logout">
+									<LogOutIcon className="size-4 mr-2" />{" "}
+									Logout
+								</Link>
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
+			) : (
+				<div className="flex items-center gap-4">
+					<Button asChild variant="outline">
+						<Link to="/auth/login">Login</Link>
+					</Button>
+					<Button asChild>
+						<Link to="/auth/signup">Join</Link>
+					</Button>
+				</div>
+			)}
 		</nav>
 	);
 }
